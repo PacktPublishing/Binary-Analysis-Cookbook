e5981e4a7bd6c9bc718196e3a1383388c66078b8
blu3gl0w13 <blu3gl0w13@gmail.com>
2019-02-06 06:16:12 -0600

    Chapter-03 code

---
+++ Chapter-03/64bit/ch03-example.c
@@ -0,0 +1,40 @@
+
+
+
+
+
+/*
+ch03-example.c
+by Michael Born
+date: 2019
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+ 
+#define HIDEIT(a)	(a) ^ 0xab
+#define UNHIDE(a)	(a) ^= 0xab
+
+int main(void) {
+
+	char hardCodedPassword[] = "HardCodedPassword";
+	char hiddenMessage[] ={ HIDEIT('S'), HIDEIT('e'), HIDEIT('c'), HIDEIT('r'), HIDEIT('e'), HIDEIT('t'), HIDEIT(' '), HIDEIT('M'), HIDEIT('e'), HIDEIT('s'), HIDEIT('s'), HIDEIT('a'), HIDEIT('g'), HIDEIT('e'), '\0' };
+	char userInput[sizeof(hardCodedPassword)];
+
+	printf("Please enter the password to unlock the hidden message: ");
+	scanf("%s", userInput);
+	if(strcmp(userInput,hardCodedPassword) == 0) {
+		for(int i = 0;i < sizeof(hiddenMessage);i++) {
+			if(hiddenMessage[i] != '\0') {
+				UNHIDE(hiddenMessage[i]);
+			}
+		}
+		printf("The hidden message is \'%s\'\n", hiddenMessage);
+	}
+	else {
+		printf("Wrong, run the program again...");
+	}
+	return 0;
+}
