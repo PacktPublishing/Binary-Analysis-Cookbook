3952d84b393f1b245696ff3888a01f3f0a395f84
blu3gl0w13 <blu3gl0w13@gmail.com>
2019-02-12 08:12:12 -0600

    ch03 32bit shared lib example

---
+++ Chapter-03/32bit/ch03-example2.c
@@ -0,0 +1,25 @@
+/*
+
+ch03-example2.c - demonstrate the use of shared libraries
+by: Michael Born
+Date: 2019
+
+*/
+
+#include <stdio.h>
+
+
+int main(void) {
+
+	char hiddenMessage[] = {hideMe('H'), hideMe('i'), hideMe('d'), hideMe('d'), hideMe('e'), hideMe('n'), '\0'};
+	printf("The hidden message is %s\n", hiddenMessage);
+
+	for(int i = 0;i < sizeof(hiddenMessage); i++) {
+		if(hiddenMessage[i] != '\0') {
+			hiddenMessage[i] = unhideMe(hiddenMessage[i]);
+		}
+	}
+
+	printf("The unhidden message is %s\n", hiddenMessage);
+	return 0;
+}
---
+++ Chapter-03/32bit/libhider.c
@@ -0,0 +1,23 @@
+/* 
+
+xorlib.c - quick xor functions for demonstrating iteration
+by Michael born
+Date 2019
+
+*/
+
+#include <stdio.h>
+
+
+char hideMe(char a) {
+
+	return a ^ 0xab;
+}
+
+
+char unhideMe(char b) {
+
+	b ^= 0xab;
+	return b;
+}
+
