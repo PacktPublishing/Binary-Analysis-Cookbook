13dfdb804e0c3abe06711aa90d7e6efe26ecf548
blu3gl0w13 <blu3gl0w13@gmail.com>
2019-06-12 05:45:21 -0500

    chapter08 32bit code

---
+++ Chapter-08/32bit/bac-automation.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# BINARY ANALYSIS AUTOMATION
+# BY - Michael Born
+# DATE - 2019
+
+
+if [ $# -lt 2 ] || [$# -gt 2 ]
+then
+    echo "Not enough arguments: usage = $0 <binary file> <output file>";
+
+else
+    BINARY=$1;
+    OUTPUTFILE="$2-$(date +%m-%d-%Y-%H:%M:%S).txt";
+
+    # SETUP OUTPUT INFORMATION
+    echo "This output created by $USER on $(date)" |tee $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+
+   # FILE
+    echo "*** FILE TYPE INFORMATION ***" | tee $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE
+    file $BINARY |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+
+    # STRINGS
+    echo "*** STRINGS INFORMATION ***" |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+    strings -s "  |  " $BINARY |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE
+
+    # READELF
+    echo "*** READELF ALL ***" |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+    readelf -a -W $BINARY |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+
+    # OBJDUMP
+    echo "*** OBJDUMP EXECUTABLE ***" |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE
+    objdump -D -M intel $BINARY |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+    echo |tee -a $OUTPUTFILE;
+fi
